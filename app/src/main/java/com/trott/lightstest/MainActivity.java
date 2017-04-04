ca741678cdce1e87eac469bb4f69fffbe5ae512e
Xunrel <d.trott87@gmail.com>
2017-04-04 23:12:54 +0200

    - Adjusted  duration of animation

--- app/src/main/java/com/trott/lightstest/MainActivity.java
+++ app/src/main/java/com/trott/lightstest/MainActivity.java
@@ -1,16 +1,22 @@
 package com.trott.lightstest;
 
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -39,12 +45,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private boolean _isRegistering = false;
     private Random _random;
     private ArrayList<Float> _lxValues;
+    //Variable to store brightness value
+    private int brightness;
+    private int _userBrightness;
+    private int _userBrightnessMode;
+    //Content resolver used as a handle to the system's settings
+    private ContentResolver cResolver;
+    //Window object, that will store a reference to the current window
+    private Window window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            setBrightness();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         _manager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         _lightSensor = _manager.getDefaultSensor(Sensor.TYPE_LIGHT);
@@ -60,6 +80,46 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         _lxValues = new ArrayList<>();
         _random = new Random();
         createBlinkAnimation();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void setBrightness(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.System.canWrite(getApplicationContext())) {
+//                ContentResolver cResolver = this.getApplicationContext().getContentResolver();
+//                Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, 255);
+//                refreshWindow();
+            } else {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }
+        cResolver = this.getApplicationContext().getContentResolver();
+        try {
+            _userBrightnessMode = Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS_MODE);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        _userBrightness = Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, 255);
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, 255);
+        refreshWindow();
+    }
+
+    private void refreshWindow() {
+        try {
+            int br = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);  //this will get the information you have just set...
+
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.screenBrightness = (float) br / 255; //...and put it here
+            getWindow().setAttributes(lp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Refreshing window", e.toString());
+        }
     }
 
     @Override
@@ -78,16 +138,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     protected void onPause() {
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, _userBrightnessMode);
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, _userBrightness);
         super.onPause();
         _manager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, _userBrightnessMode);
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, _userBrightness);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, _userBrightnessMode);
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, _userBrightness);
+        super.onStop();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         _manager.registerListener(this, _lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        int minDelay = _lightSensor.getMinDelay();
-//        int maxDelay = _lightSensor.getMaxDelay();
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, 255);
     }
 
     public void toggleRegistering(View view) {
@@ -126,7 +204,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private void createBlinkAnimation() {
         _blinkAnimation = new AlphaAnimation(1, 0);
         _blinkAnimation.setInterpolator(new LinearInterpolator());
+//        AccelerateInterpolator dec = new AccelerateInterpolator(5f);
+//        _blinkAnimation.setInterpolator(dec);
         _blinkAnimation.setDuration(170);
+//        _blinkAnimation.setDuration(1000);
         _blinkAnimation.setRepeatCount(Animation.INFINITE);
         _blinkAnimation.setRepeatMode(Animation.REVERSE);
 
